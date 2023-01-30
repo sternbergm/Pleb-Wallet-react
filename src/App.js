cef815eabf59c9f8b3ad294d16d600dcb3c43a0f
Misha Sternberg <mishasternberg@gmail.com>
2023-01-30 16:58:59 -0500

    Initial Project Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [price, setPrice] = useState(0);
+
+  const getPrice = () => {
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      .then((res) => {
+        setPrice(res.data.data.amount);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The current BTC price is ${price}</h1>
     </div>
   );
 }
